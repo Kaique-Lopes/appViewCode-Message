f02c86869c076a0364b9835264610faa8d6bc467
Kaique <kaique.lopes3271@gmail.com>
2022-05-31 17:15:40 -0300

    delete story board, create LoginViewController
    delete story board,
    create LoginViewController

--- appViewCode-Message/appViewCode-Message/LoginViewController.swift
+++ appViewCode-Message/appViewCode-Message/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  appViewCode-Message
 //
 //  Created by Kaique Lopes de Oliveira on 31/05/22.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
 
 
