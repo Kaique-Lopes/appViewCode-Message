c478cb4f24248eb0b1369fd1287b5ebe1778b007
Kaique <kaique.lopes3271@gmail.com>
2022-06-03 00:12:22 -0300

    create keyboardType and isSecureTextEntry

--- appViewCode-Message/appViewCode-Message/Components/UITextFieldDefault.swift
+++ appViewCode-Message/appViewCode-Message/Components/UITextFieldDefault.swift
@@ -9,19 +9,23 @@ import Foundation
 import UIKit
 
 class UITextFieldDefault: UITextField {
-    init(text: String) {
+    init(text: String, keyboardType: UIKeyboardType = .default, isSecure: Bool = false) {
         super.init(frame: .zero)
-        
         initDefault(text: text)
+        self.keyboardType = keyboardType
+        self.isSecureTextEntry = isSecure
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func initDefault(text: String) {
+   private func initDefault(text: String) {
         self.translatesAutoresizingMaskIntoConstraints = false
-        self.backgroundColor = .cyan
+        self.backgroundColor = .white
         self.placeholder = text
+        self.autocorrectionType = .no
+        self.borderStyle = .roundedRect
+        self.textColor = .darkGray
     }
 }
--- appViewCode-Message/appViewCode-Message/Login/Screen/LoginScreen.swift
+++ appViewCode-Message/appViewCode-Message/Login/Screen/LoginScreen.swift
@@ -12,8 +12,8 @@ class LoginScreen: UIView {
 // MARK: - Components
     lazy var loginLabel = LabelDefault(text: "Login")
     lazy var logoImage = UIImageDefault(imageName: "logo")
-    lazy var loginTextField = UITextFieldDefault(text: "Login")
-    lazy var emailTextField = UITextFieldDefault(text: "Email")
+    lazy var loginTextField = UITextFieldDefault(text: "Email", keyboardType: .emailAddress)
+    lazy var passwordTextField = UITextFieldDefault(text: "Senha", keyboardType: .default, isSecure: true)
     
     // MARK: - Overrides
     override init(frame: CGRect) {
@@ -35,7 +35,16 @@ class LoginScreen: UIView {
             self.logoImage.topAnchor.constraint(equalTo: self.loginLabel.bottomAnchor, constant: 20),
             self.logoImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
             self.logoImage.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            self.logoImage.heightAnchor.constraint(equalToConstant: 200)
+            self.logoImage.heightAnchor.constraint(equalToConstant: 200),
+            
+            self.loginTextField.topAnchor.constraint(equalTo: self.logoImage.bottomAnchor, constant: 20),
+            self.loginTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            self.loginTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            
+            self.passwordTextField.topAnchor.constraint(equalTo: self.loginTextField.bottomAnchor, constant: 20),
+            self.passwordTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            self.passwordTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+
         ])
     }
     
@@ -44,7 +53,7 @@ class LoginScreen: UIView {
         self.addSubview(loginLabel)
         self.addSubview(logoImage)
         self.addSubview(loginTextField)
-        self.addSubview(emailTextField)
+        self.addSubview(passwordTextField)
     }
     
 }
